660faa9e46666953388021241bc36ad8f0681136
izauranathania-web <izauranathania@gmail.com>
2026-06-01 21:42:13 +0700

    mengatasi eror csrf login

--- tani_project/settings.py
+++ tani_project/settings.py
@@ -71,3 +71,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3f027.up.railway.app']
